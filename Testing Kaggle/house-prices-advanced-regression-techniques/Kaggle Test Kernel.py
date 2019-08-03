73e561e57ae68e61d85eb4bdb0c0ebed29041de1
AntonYurievNikolov <antonyurievnikolov@gmail.com>
2019-08-03 14:49:41 +0300

    My first Kaggle Kernel, top 20%

---
+++ Testing Kaggle/house-prices-advanced-regression-techniques/Kaggle Test Kernel.py
@@ -0,0 +1,130 @@
+# Import xgboost
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense,BatchNormalization
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
+from keras import optimizers
+#
+#train = pd.read_csv('../input/train.csv')
+#test = pd.read_csv('../input/test.csv')
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+#We do not need the ID column
+train_ID = train['Id']
+test_ID = test['Id']
+train.drop("Id", axis = 1, inplace = True)
+test.drop("Id", axis = 1, inplace = True)
+
+categorical_mask = (train.dtypes == object)
+categorical_columns = train.columns[categorical_mask].tolist()
+non_categorical_columns = train.columns[~categorical_mask].tolist()
+
+###START OF FEATURE ENGINEERING  taken from "Stacked Regressions : Top 4% on LeaderBoard" Kernel
+#Remove ouliers
+train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
+#SCALE THE TARGET
+train["SalePrice"] = np.log1p(train["SalePrice"])
+
+ntrain = train.shape[0]
+ntest = test.shape[0]
+y_train = train.SalePrice.values
+all_data = pd.concat((train, test),sort=True).reset_index(drop=True)
+all_data.drop(['SalePrice'], axis=1, inplace=True)
+
+#None if there is no Pool
+all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
+all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
+all_data["Alley"] = all_data["Alley"].fillna("None")
+all_data["Fence"] = all_data["Fence"].fillna("None")
+all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
+
+all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
+    lambda x: x.fillna(x.median()))
+
+for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    all_data[col] = all_data[col].fillna('None')
+
+for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF','TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+    all_data[col] = all_data[col].fillna(0)
+
+for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+    all_data[col] = all_data[col].fillna('None')
+all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+all_data = all_data.drop(['Utilities'], axis=1)
+all_data["Functional"] = all_data["Functional"].fillna("Typ")
+all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
+all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
+all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
+all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
+all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
+all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
+
+#Adding Features 
+all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+all_data = pd.get_dummies(all_data)
+train = all_data[:ntrain]
+test = all_data[ntrain:]
+
+####END OF FEATURE ENGINEERING
+X = train
+y =  y_train
+
+###Model taken from "Stacked Regressions : Top 4% on LeaderBoard" Kernel
+xgbreg=xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
+                             learning_rate=0.05, max_depth=3, 
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state =7, nthread = -1)
+
+
+xgbreg.fit(X, y)
+xgb_train_pred = xgbreg.predict(X)
+xgb_pred = np.expm1(xgbreg.predict(test))
+print(np.sqrt(mean_squared_error(y_train, xgb_train_pred)))
+
+def get_new_model ():
+    n_cols =X.shape[1]
+    model = Sequential()
+    model.add(BatchNormalization(input_shape = (n_cols,)))
+    model.add(Dense(50, activation='relu' ))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(1))
+    #model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+    
+#TESTING FEW LR
+#Optimizing the model
+lr_to_test = [0.001, 0.002, 0.003]
+early_stopping_monitor = EarlyStopping(patience=2)
+
+#for lr in lr_to_test:
+#    print('\n\nTesting model with learning rate: %f\n'%lr )
+#    model = get_new_model()
+#    my_optimizer = optimizers.Adam(lr=lr)
+#    model.compile(optimizer=my_optimizer, loss='mean_squared_error',metrics=['mse'])#For Categories and softmax metrics=['accuracy'])
+#    model.fit(X, y,epochs=10, validation_split=0.3,callbacks=[early_stopping_monitor])
+# 
+keras_model = get_new_model()  
+my_optimizer = optimizers.Adam(lr=0.001)
+keras_model.compile(optimizer=my_optimizer, loss='mean_squared_error',metrics=['mse'])#For Categories and softmax metrics=['accuracy'])
+keras_model.fit(X, y,epochs=200,callbacks=[early_stopping_monitor])
+keras_pred=np.expm1(keras_model.predict(test))
+
+keras_train = keras_model.predict(X)
+print(np.sqrt(mean_squared_error(y, keras_train)))
+
+ensemble = 0.5*keras_pred+0.5*xgb_pred
+sub = pd.DataFrame()
+sub['Id'] = test_ID
+sub['SalePrice'] = ensemble
+sub.to_csv('submission.csv',index=False)
--- XGBOOST/house-prices-advanced-regression-techniques/Case Study With Pipes.py
+++ XGBOOST/house-prices-advanced-regression-techniques/Case Study With Pipes.py
@@ -6,6 +6,9 @@ from sklearn.metrics import mean_squared_error,mean_squared_log_error
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.pipeline import Pipeline
+
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import FeatureUnion
 from sklearn_pandas import DataFrameMapper
 from sklearn_pandas import CategoricalImputer
 from sklearn.impute import SimpleImputer
@@ -14,91 +17,106 @@ import pandas as pd
 import numpy as np
 
 
+#
+#train = pd.read_csv('../input/train.csv')
+#test = pd.read_csv('../input/test.csv')
 
-
-df = pd.read_csv("train.csv")
+train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
+#We do not need the ID column
+train_ID = train['Id']
+test_ID = test['Id']
+train.drop("Id", axis = 1, inplace = True)
+test.drop("Id", axis = 1, inplace = True)
 
 
-
-categorical_mask = (df.dtypes == object)
-categorical_columns = df.columns[categorical_mask].tolist()
-non_categorical_columns = df.columns[~categorical_mask].tolist()
+categorical_mask = (train.dtypes == object)
+categorical_columns = train.columns[categorical_mask].tolist()
+non_categorical_columns = train.columns[~categorical_mask].tolist()
 
 
 #X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=123)
 #Quick and durty - this will be extended with meaningfull fill for the missing data
-df[categorical_columns] = df[categorical_columns].fillna('')
-df[non_categorical_columns] = df[non_categorical_columns].fillna(0)
 
+train[categorical_columns] = train[categorical_columns].fillna('')
+#train[non_categorical_columns] = train[non_categorical_columns].fillna(train.mean())
 
-X = df.iloc[:,:-1]
-y =  df.iloc[:,-1]
+####Qick Test Remove later
+#Remove ouliers
+train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
+#Normilize the Target
+#train["SalePrice"] = np.log1p(train["SalePrice"])
+
+X = train.iloc[:,:-1]#pd.get_dummies() 
+y =  train.iloc[:,-1]
 #nulls_per_column = X.isnull().sum()
 #print(nulls_per_column)
 #print(nulls_per_column.sum())
+###BUILD THE PIPE
+get_text_data = FunctionTransformer(lambda x: x[categorical_columns], validate=False)
+get_numeric_data = FunctionTransformer(lambda x: x[non_categorical_columns], validate=False)
 
-numeric_imputation_mapper = DataFrameMapper(
-                                            [([numeric_feature],SimpleImputer(strategy="median")) for numeric_feature in non_categorical_columns],
-                                            input_df=True,
-                                            df_out=True
-                                           )
-categorical_imputation_mapper = DataFrameMapper(
-                                                [(category_feature, CategoricalImputer()) for category_feature in categorical_columns],
-                                                input_df=True,
-                                                df_out=True
-                                               )
+xgbreg=xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
+                             learning_rate=0.05, max_depth=3, 
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state =7, nthread = -1)
 
-numeric_categorical_union = FeatureUnion([
-                                          ("num_mapper", numeric_imputation_mapper),
-                                          ("cat_mapper", categorical_imputation_mapper)
-                                         ])
+process_and_join_features = FeatureUnion(
+            transformer_list = [
+                ('numeric_features', Pipeline([
+                    ('selector', get_numeric_data),
+                    ('imputer', SimpleImputer()),
+                    ("std_scaler", StandardScaler(with_mean=False))
+                ])),
+                ('text_features', Pipeline([
+                    ('selector', get_text_data),
+#                    ('OneHot', pd.get_dummies )
+                ]))
+             ]
+        )
 
-transformers = []
-transformers.extend([([numeric_feature], [SimpleImputer(strategy="median"), 
-                                  StandardScaler()]) for numeric_feature in non_categorical_columns])
-transformers.extend([(category_feature , [CategoricalImputer()]) for category_feature in categorical_columns])
-
-num_cat_union = DataFrameMapper(transformers,
-                                input_df=True,
-                                df_out=True)
-# Create full pipeline
 pipeline = Pipeline([
-#                     ("featureunion", num_cat_union),
-#                     ("dictifier", Dictifier()),
-                     ("vectorizer", DictVectorizer(sort=False)),
-#                     ("std_scaler", StandardScaler(with_mean=False)),
-                     ("clf", xgb.XGBRegressor(objective = "reg:squarederror"))
-                    ])
+        ('union', process_and_join_features),
+         ("ohe_onestep", DictVectorizer(sort=False)),
+        ('clf', xgbreg)
+    ])
 
+
+
+## Create full pipeline
+pipeline = Pipeline([
+                     ("ohe_onestep", DictVectorizer(sort=False)),
+                     ("clf",xgbreg)
+                    ])
+cv = 3
 cross_val_scores = cross_val_score(pipeline, 
                                     X.to_dict("records"), 
                                     y, 
-                                    cv=3, 
+                                    cv=cv, 
                                     scoring="neg_mean_squared_error")
-print("3-fold AUC: ", np.mean(cross_val_scores))
+print(cv,"-fold RMSE: ", np.mean(np.sqrt(np.abs(cross_val_scores))))
 
 
-# =============================================================================
-# # Create the parameter grid
-# gbm_param_grid = {
+# Create the parameter grid
+#gbm_param_grid = {
 #     'clf__learning_rate': np.arange(.05, 1, .05),
 #     'clf__max_depth': np.arange(3,10, 1),
 #     'clf__n_estimators': np.arange(50, 200, 50)
 # }
 # 
-# # Perform RandomizedSearchCV
-# randomized_neg_mean_squared_error = RandomizedSearchCV(estimator=pipeline,
+## Perform RandomizedSearchCV
+#randomized_neg_mean_squared_error = RandomizedSearchCV(estimator=pipeline,
 #                                         param_distributions=gbm_param_grid,
-#                                         n_iter=2, scoring='neg_mean_squared_error', cv=2, verbose=1)
+#                                         n_iter=9, scoring='neg_mean_squared_error', cv=2, verbose=1)
 # 
-# # Fit the estimator
-# randomized_neg_mean_squared_error.fit(X, y)
+## Fit the estimator
+#randomized_neg_mean_squared_error.fit(X.to_dict("records"), y)
 # 
-# # Compute metrics
-# print(randomized_neg_mean_squared_error.best_score_)
-# print(randomized_neg_mean_squared_error.best_estimator_)
-# =============================================================================
+## Compute metrics
+#print(np.mean(np.sqrt(np.abs(randomized_neg_mean_squared_error.best_score_))))
+#print(randomized_neg_mean_squared_error.best_estimator_)
 
 ##Encode the Categorical Data
 #print(df[categorical_columns].head())
@@ -121,18 +139,3 @@ print("3-fold AUC: ", np.mean(cross_val_scores))
 #print(df_encoded[:5,:])
 #print(dv.vocabulary_)
 
-  #Working Test with Pipes
-steps = [("ohe_onestep", DictVectorizer(sparse=False)),
-           ("xgb_model", xgb.XGBRegressor(objective = "reg:squarederror"))]
-  
-xgb_pipeline = Pipeline(steps)
-  #xgb_pipeline.fit(X.to_dict("records"), y)
-  #predict = xgb_pipeline.predict(X.to_dict("records"))
-  
-  
-cross_val_scores = cross_val_score(xgb_pipeline, 
-                                     X.to_dict("records"), 
-                                     y, 
-                                     cv=2, 
-                                     scoring="neg_mean_squared_error")
-print("10-fold RMSE: ", np.mean(np.sqrt(np.abs(cross_val_scores))))
