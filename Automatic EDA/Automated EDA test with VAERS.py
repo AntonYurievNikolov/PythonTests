911a371046ff3ac56d4996eb46120a5dd1864662
AntonYurievNikolov <AntonYurievNikolov@gmail.com>
2022-09-21 11:13:59 +0300

    small fix

--- Automatic EDA/Automated EDA test with VAERS.py
+++ Automatic EDA/Automated EDA test with VAERS.py
@@ -15,9 +15,9 @@ sweet_report.show_html('sweetviz_report.html')
 df1 = sv.compare(df[210:], df[:90])
 df1.show_html('sweetvizCompare2.html')
 
-#autoviz
-from autoviz.AutoViz_Class import AutoViz_Class
-AV = AutoViz_Class()
-df = AV.AutoViz('2020VAERSDATA.csv')
+#autoviz - generates the report directly here
+# from autoviz.AutoViz_Class import AutoViz_Class
+# AV = AutoViz_Class()
+# df = AV.AutoViz('2020VAERSDATA.csv')
 
-df = AV.AutoViz('2020VAERSDATA.csv', depVar='DIED')
+# df = AV.AutoViz('2020VAERSDATA.csv', depVar='DIED')
